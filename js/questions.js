095d8639d4f24dbd3c0309ba0f9dd7d71851459e
Otis Valentine <cashp85@gmail.com>
2024-01-20 21:48:15 -0500

    How Jamaican Are You? Quiz Game

---
+++ js/questions.js
@@ -0,0 +1,237 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "On what day is Jamaican Independence observed each year?", 
+    answer: "August 6 (or, in case it falls on a Sunday, then the following day)",
+    options: [
+         "The first Monday of August",
+         "August 6 (or, in case it falls on a Sunday, then the following day)",
+         "August 7",
+         "Government decides on a date each year"
+    ]
+},
+{
+    numb: 2,
+    question: "Who is Jamaica’s first National Hero?", 
+    answer: "Marcus Garvey",
+    options: [
+         "Alexander Bustamante",
+         "Bob Marley",
+         "Marcus Garvey",
+         "Nanny of the Maroons"
+    ]
+},
+{
+    numb: 3,
+    question: "Which National Hero was the leader of Morant Bay Rebellion?", 
+    answer: "Paul Bogle",
+    options: [
+         "Paul Bogle",
+         "Nanny of the Maroons",
+         "Sam Sharpe",
+         "George William Gordon "
+    ]
+},
+{
+    numb: 4,
+    question: "Who represented The Queen at Jamaica’s Independence celebrations in 1962?", 
+    answer: "Princess Margaret",
+    options: [
+         "No one, Queen Elizabeth II was present at the celebration",
+         "Princess Margaret",
+         "Governor General, Sir Kenneth Blackburne",
+         "Prince Philip"
+    ]
+},
+{
+    numb: 5,
+    question: "What is the National Bird?", 
+    answer: "‘Doctor Bird’",
+    options: [
+         "Chicken",
+         "‘Doctor Bird’",
+         "John Crow",
+         "Yellow-billed Parrot"
+    ]
+}, 
+{
+    numb: 6,
+    question: "What is Jamaica’s National Fruit?",
+    answer: "Ackee",
+    options: [
+         "Ackee",
+         "Jamaica does not have a National Fruit",
+         "Pineapple",
+         "Mango"
+    ]
+},
+{
+    numb: 7,
+    question: "Bob Marley lived in… when he met 'The Wailers'", 
+    answer: "Trench Town, Kingston",
+    options: [
+         "Morant Bay, St Thomas",
+         "Nine Mile, St Ann",
+         "Trench Town, Kingston",
+         "Oracabessa, St Mary"
+    ]
+},
+{
+    numb: 8,
+    question: "Which one of the following is not a code for use of the Jamaican Flag?",
+    answer: "All of the above are codes for use of the Jamaican flag.",
+    options: [
+         "The Jamaican flag should never be allowed to touch the ground or floor.",
+         "When the flag becomes worn and must be replaced, it should be burnt privately",
+         "All of the above are codes for use of the Jamaican flag.",
+         "The flag should never be smaller than any other flag flown at the same time."
+    ]
+},
+{
+    numb: 9,
+    question: "Who was Jamaica’s first Prime Minister?",
+    answer: "Alexander Bustamante",
+    options: [
+         "Norman Manley",
+         "Alexander Bustamante",
+         "Michael Manley",
+         "Donald Sangster"
+    ]
+},
+{
+    numb: 10,
+    question: "Who are believed to be the original inhabitants of Jamaica?",
+    answer: "Tainos",
+    options: [
+         "Africans",
+         "Tainos",
+         "Caribs",
+         "The Spanish"
+    ]
+},
+{
+    numb: 11,
+    question: "Where in Jamaica was infamously known as the ‘wickedest city on Earth’?", 
+    answer: "Port Royal",
+    options: [
+         "Tivoli Gardens",
+         "De La Vega City",
+         "Port Royal",
+         "Cherry Gardens"
+    ]
+},
+{ 
+    numb: 12,
+    question: "What high school did Usain Bolt attend?",
+    answer: "William Knibb",
+    options: [
+         "Munro",
+         "Calabar",
+         "Kingston College",
+         "William Knibb"
+    ]
+},
+{
+    numb: 13,
+    question: "Who won the first Festival Song Competition in 1966?",
+    answer: "The Maytals – ‘Bam Bam’",
+    options: [
+         " Eric Donaldson – ‘Cherry Oh Baby’",
+         "The Maytals – ‘Bam Bam’",
+         "The Jamaicans – ‘Ba Ba Boom’",
+         "Roy Rayon – ‘Give Thanks and Praises’"
+    ]
+},
+{        
+    numb: 14,
+    question: "How many parishes are in Jamaica?", 
+    answer: "14",
+    options: [
+         "10",
+         "12",
+         "14",
+         "15"
+    ]
+},
+{        
+    numb: 15,
+    question: "Which parish is known as the “bread basket parish”?", 
+    answer: "St Elizabeth",
+    options: [
+         "St Elizabeth",
+         "St Ann",
+         "Portland",
+         "St. Thomas"
+    ]
+},
+{        
+    numb: 16,
+    question: "What was the capital city of Jamaica immediately before Kingston became capital in 1872?", 
+    answer: "Spanish Town",
+    options: [
+         "Montego Bay",
+         "Spanish Town",
+         "Port Royal",
+         "Seville"
+    ]
+},
+{        
+    numb: 17,
+    question: "Who was the first Jamaican woman to win an Olympic Gold medal?", 
+    answer: "Deon Hemmings McCatty",
+    options: [
+         "Veronica Campbell Brown",
+         "Deon Hemmings McCatty",
+         "Grace Jackson",
+         "Merlene Ottey"
+    ]
+},
+{        
+    numb: 18,
+    question: "Who won the first Grammy Award for Best Reggae Album?", 
+    answer: "Black Uhuru – ‘Anthem’",
+    options: [
+         "Black Uhuru – ‘Anthem’",
+         "Bob Marley – ‘Legend’",
+         "Shabba Ranks – ‘As Raw As Ever’",
+         "Ziggy Marley and the Melody Makers – ‘Conscious Party’"
+    ]
+},
+{        
+    numb: 19,
+    question: "Who scored Jamaica’s first ever win, in a FIFA World Cup Match?", 
+    answer: "Theodore Whitmore",
+    options: [
+         "Theodore Whitmore",
+         "Robbie Earle",
+         "Walter Boyd",
+         "Deon Burton"
+    ]
+},
+{        
+    numb: 20,
+    question: "Who was the first Jamaican to win the Miss World contest?", 
+    answer: "Carole Joan Crawford",
+    options: [
+         "Lisa Hanna",
+         "Carole Joan Crawford",
+         "Cindy Breakspeare",
+         "Yendi Phillipps"
+    ]
+},
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
